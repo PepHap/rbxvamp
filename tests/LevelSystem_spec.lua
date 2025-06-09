44f93527d59a5fc9e06a38c87803d9746d9e825a
PepHap <160134716+PepHap@users.noreply.github.com>
2025-06-09 19:39:27 +0300

    Add spec for system registration

--- tests/LevelSystem_spec.lua
+++ tests/LevelSystem_spec.lua
@@ -1,5 +1,9 @@
-local LevelSystem = require("../src/LevelSystem")
-local EnemySystem = require("../src/EnemySystem")
+-- Use the same module paths as the production code so that the
+-- EnemySystem instance referenced here matches the one used within
+-- LevelSystem. This prevents state from diverging between two copies
+-- of the module when the tests run.
+local LevelSystem = require("src.LevelSystem")
+local EnemySystem = require("src.EnemySystem")
 
 describe("LevelSystem", function()
     it("starts at level 1", function()
